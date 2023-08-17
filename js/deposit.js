29764359d1a3f2541630039db4f73387d3902676
iamapusaha <apusaha392@gmail.com>
2023-08-17 18:14:14 +0600

    added to function for get input and element value

--- js/deposit.js
+++ js/deposit.js
@@ -1,3 +1,17 @@
+function getInputValueById(inputId) {
+    const inputField = document.getElementById(inputId);
+    const inputFieldValueString = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldValueString);
+    return inputFieldValue;
+}
+
+function getElementValueById(elementId) {
+    const element = document.getElementById(elementId);
+    const elementValueString = element.innerText;
+    const elementValue = parseFloat(elementValueString)
+}
+
 document.getElementById('btn-deposit').addEventListener('click', function () {
-    console.log('btn clicked');
+    const newDepositAmount = getInputValueById('deposit-field');
+    console.log(newDepositAmount);
 })
