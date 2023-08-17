5b974e4d78024bef4534ee4131ed4daed9c4aca1
iamapusaha <apusaha392@gmail.com>
2023-08-17 22:56:17 +0600

    added withdraw functionlity

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,3 +1,10 @@
 document.getElementById('btn-withdraw').addEventListener('click', function () {
-    console.log('clicked');
+    const newWithdrawAmount = getInputValueById('withdraw-field');
+    const preWithDrawAmount = getElementValueById('withdraw-total');
+    const currentWithdrawTotal = preWithDrawAmount + newWithdrawAmount;
+    setTextElementValueById('withdraw-total', currentWithdrawTotal);
+
+    const preBalanceAmount = getElementValueById('balance-total');
+    const currentBalamceTotal = preBalanceAmount - newWithdrawAmount;
+    setTextElementValueById('balance-total', currentBalamceTotal);
 })
